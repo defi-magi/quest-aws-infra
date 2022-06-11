e9f60c536b1b3aafdf0bd6fee84c046011c88251
curiousApe <curiousape@defimagi.com>
2022-06-10 18:14:00 -0600

    Update known hosts

--- eks-quest-cluster-dev-00/bootstrap/main.tf
+++ eks-quest-cluster-dev-00/bootstrap/main.tf
@@ -1,5 +1,5 @@
 module "argocd_bootstrap" {
-  source                            = "git::ssh://source.shinra.com/aws-terraform-infra/aws-shinra-k8s-modules//core-bootstrap?ref=v1.0.2"
+  source                            = "git::git@github.com:defi-magi/quest-eks-modules.git//core-bootstrap"
   common_tags                       = var.common_tags
   tags                              = var.tags
   eks_oidc_provider_arn             = data.terraform_remote_state.cluster_core.outputs.eks_cluster_full_outputs.oidc_provider_arn
@@ -8,62 +8,4 @@ module "argocd_bootstrap" {
   external_dns_irsa                 = var.external_dns_irsa
   cert_manager_irsa                 = var.cert_manager_irsa
   external_secrets_irsa             = var.external_secrets_irsa
-}
-
-#---------------------------------------------------------------------------------------------------
-# AWS Vault Service Account IAM & EKS
-# This service account will be used to provide least privilaged access to Vault infrastructure from
-# the Vault Pods from within the cluster
-#---------------------------------------------------------------------------------------------------
-locals {
-  vault_cluster_role_name = format("%s-%s-%s-%s-%s", var.common_tags.application-id, var.common_tags.project, "cluster", "vault-cluster", var.common_tags.index)
-}
-
-data "aws_iam_policy_document" "vault_sa_role_trust_policy_document" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect  = "Allow"
-
-    condition {
-      test     = "StringEquals"
-      values   = ["system:serviceaccount:vault:vault-irsa"]
-      variable = "${replace(data.terraform_remote_state.cluster_core.outputs.eks_cluster_full_outputs.cluster_oidc_issuer_url, "https://", "")}:sub"
-    }
-
-    principals {
-      identifiers = [data.terraform_remote_state.cluster_core.outputs.eks_cluster_full_outputs.oidc_provider_arn]
-      type        = "Federated"
-    }
-  }
-}
-
-resource "aws_iam_role" "vault_sa_iam_role" {
-  name               = "${local.vault_cluster_role_name}-vault-irsa-role" #TODO: DR: Make unique, append -dr to project 
-  path               = "/vault/"
-  assume_role_policy = data.aws_iam_policy_document.vault_sa_role_trust_policy_document.json
-}
-
-resource "kubernetes_namespace" "vault" {
-  metadata {
-    annotations = {}
-    labels = {
-      "kubernetes.io/metadata.name" = "vault"
-    }
-    name = "vault"
-  }
-
-  lifecycle {
-    ignore_changes = [metadata]
-  }
-}
-
-resource "kubernetes_service_account" "vault_sa_iam" {
-  metadata {
-    name      = "vault-irsa"
-    namespace = "vault"
-
-    annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.vault_sa_iam_role.arn
-    }
-  }
 }
--- eks-quest-cluster-dev-00/bootstrap/provider-backend.tf
+++ eks-quest-cluster-dev-00/bootstrap/provider-backend.tf
@@ -18,11 +18,10 @@ provider "aws" {
 data "terraform_remote_state" "cluster_core" {
   backend = "s3"
   config = {
-    role_arn   = "arn:aws:iam::142540212645:role/811075206237-terraform-state-access"
-    bucket     = "shinra-terraform-remote-state"
-    key        = "811075206237/security-tooling/dev/kubernetes/cluster-00/terraform.tfstate"
-    kms_key_id = "arn:aws:kms:us-west-2:142540212645:alias/shinra-terraform-remote-state-backend-811075206237"
-    region     = "us-west-2"
+    bucket     = "quest-terraform-remote-state"
+    key        = "quest-infra/eks/quest/dev/00/terraform.tfstate"
+    region     = "us-east-1"
+    kms_key_id = "fd69cf79-218c-4a9e-8c56-90a25802ffa3"
   }
 }
 
